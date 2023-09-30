9508579d9a95d80338c69a59bc035959768afbba
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-30 12:17:26 -0300

    feat: divider

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Divider,
   Grid,
   List,
   ListItemButton,
@@ -18,6 +19,7 @@ export const Sidebar = () => {
           <ListItemText primary="Tarefas" />
         </ListItemButton>
       </List>
+      <Divider />
     </Grid>
   );
 };
