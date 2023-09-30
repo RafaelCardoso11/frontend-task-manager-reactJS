550d7b41de045fe6687e55975fd0fc0daf2407d4
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-30 18:00:53 -0300

    refactor: item

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -2,7 +2,7 @@ import { Grid, Link, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <Grid height="3rem">
+    <Grid  item height="3rem">
       <Typography variant="body2" color="text.secondary" align="center">
         {"Copyright © "}
         <Link color="inherit" href="https://www.linkedin.com/in/rafaelcardoso11/"  target="_blank">
