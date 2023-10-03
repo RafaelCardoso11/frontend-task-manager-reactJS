e2bb57226d6a84fdfdd2055eead9f181a60f677f
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-10-03 13:32:21 -0300

    feat: loading lazy loaded

--- src/pages/Task/index.tsx
+++ src/pages/Task/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mui/material";
+import { Button, CircularProgress, Grid } from "@mui/material";
 import { useForm, SubmitHandler, Resolver } from "react-hook-form";
 import { ITask } from "@/interfaces/task.interface";
 import { DatePicker } from "@/components/DatePicker";
@@ -34,7 +34,7 @@ export const Task: React.FC<IProps> = () => {
     resolver: yupResolver(validationSchema) as Resolver<ITask>,
   });
 
-  const { mutate: findOneTask } = useMutation(taskService.findOne, {
+  const { mutate: findOneTask, isLoading } = useMutation(taskService.findOne, {
     onSuccess(task: ITask) {
       setValue("title", task.title);
       setValue("description", task.description);
@@ -65,81 +65,87 @@ export const Task: React.FC<IProps> = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <InputText
-            control={control}
-            name="title"
-            label="Titulo"
-            disabled={isView}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <InputText
-            control={control}
-            name="description"
-            label="Descrição da Tarefa"
-            multiline
-            minRows={4}
-            disabled={isView}
-          />
-        </Grid>
+    <div>
+      {!isLoading ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <InputText
+                control={control}
+                name="title"
+                label="Titulo"
+                disabled={isView}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputText
+                control={control}
+                name="description"
+                label="Descrição da Tarefa"
+                multiline
+                minRows={4}
+                disabled={isView}
+              />
+            </Grid>
 
-        <Grid item xs={6}>
-          <Select
-            control={control}
-            name="priority"
-            label="Prioridade"
-            items={priorityItems}
-            disabled={isView}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <DatePicker
-            control={control}
-            name="dueDate"
-            minDate={dayjs()}
-            label="Data de Conclusão"
-            disabled={isView}
-          />
-        </Grid>
+            <Grid item xs={6}>
+              <Select
+                control={control}
+                name="priority"
+                label="Prioridade"
+                items={priorityItems}
+                disabled={isView}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <DatePicker
+                control={control}
+                name="dueDate"
+                minDate={dayjs()}
+                label="Data de Conclusão"
+                disabled={isView}
+              />
+            </Grid>
 
-        <Grid container margin={2} justifyContent="flex-end" spacing={1}>
-          <Grid item>
-            <Button
-              variant="contained"
-              size="large"
-              color="warning"
-              onClick={() => navigate(-1)}
-            >
-              Voltar
-            </Button>
-          </Grid>
-          <Grid item>
-            {!isView &&
-              (idTask ? (
+            <Grid container margin={2} justifyContent="flex-end" spacing={1}>
+              <Grid item>
                 <Button
                   variant="contained"
-                  type="submit"
                   size="large"
-                  color="primary"
+                  color="warning"
+                  onClick={() => navigate(-1)}
                 >
-                  Editar Tarefa
+                  Voltar
                 </Button>
-              ) : (
-                <Button
-                  variant="contained"
-                  type="submit"
-                  size="large"
-                  color="primary"
-                >
-                  Cadastrar Tarefa
-                </Button>
-              ))}
+              </Grid>
+              <Grid item>
+                {!isView &&
+                  (idTask ? (
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      size="large"
+                      color="primary"
+                    >
+                      Editar Tarefa
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      size="large"
+                      color="primary"
+                    >
+                      Cadastrar Tarefa
+                    </Button>
+                  ))}
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
-    </form>
+        </form>
+      ) : ( 
+        <CircularProgress />
+      )}
+    </div>
   );
 };
